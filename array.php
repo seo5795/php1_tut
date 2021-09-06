61e6d16bfbd6384dc80fa3c09a8183fa1b0bf9d6
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-09-06 14:01:37 +0900

    echo를 이용하여 변수를 출력하고 싶을 때 ""밖에 사용하거나 {}를 이용하여 사용한다.

--- array.php
+++ array.php
@@ -8,13 +8,16 @@
 
 
 <?php $coworkers=array('아리','티모','가렌'); 
-    echo $coworkers[0].'<br>';
+	$str="문자열";
+	echo "{$str}을 출력중입니다 <br>";
+    echo "{$coworkers[0]} <br>";
     echo $coworkers[2].'<br>';
+    //echo를 이용하여 변수를 출력하고 싶을 때 ""밖에 사용하거나 {}를 이용하여 사용한다.
+    
     var_dump(count($coworkers)); 
    
     array_push($coworkers, '홍길동');
     var_dump($coworkers);
 ?>
-
 </body>
 </html>
